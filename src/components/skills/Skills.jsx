08472421925059e469fab235a3ac8049ad48dcc0
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-28 20:06:47 +0200

    use Axios in frontend component

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './skill.css';
+import Frontend from './Frontend';
+import CS from './CS';
 const Skills = () => {
   return (
-    <div id='Skills'>Skills</div>
+    <section className='skills section' id='Skills'>
+      <h2 className="section__title" title='About Section Title'>Skills</h2>
+      <span className="section__subtitle">My technical level</span>
+      <div className="skills__container container grid">
+        <Frontend />
+        <CS />
+      </div>
+    </section>
   )
 }
 
