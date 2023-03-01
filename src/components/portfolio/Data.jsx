e4ed0edccec9a587899cb84ed344962aa4e46d57
Alaa Yasser <alaayasser2018@gmail.com>
2023-03-01 22:21:03 +0200

    add Portfolio Slider component

---
+++ src/components/portfolio/Data.jsx
@@ -0,0 +1,101 @@
+import Work1 from "../../assets/premedi.png"
+import Work2 from "../../assets/BookStore.png"
+import Work3 from "../../assets/GroxiStore.png"
+import Work4 from "../../assets/StartBootstrap.png"
+import Work5 from "../../assets/prev.png"
+import Work6 from "../../assets/HooBanck.png"
+import Work7 from "../../assets/Leon.png"
+import Work8 from "../../assets/Aroma.jpg"
+import Work9 from "../../assets/QuizApp.png"
+import Work10 from "../../assets/Cis.png"
+import Work11 from "../../assets/calc.png"
+import Work12 from "../../assets/tip.png"
+import Work13 from "../../assets/clipboard.png"
+import Work14 from "../../assets/fourCard.png"
+
+export const Data = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Premedi",
+        demo: "https://alaayasser741.github.io/premedi/",
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Book Store",
+        demo: "https://bookstoreapis.netlify.app/",
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Groxi Store",
+        demo: "https://alaayasser741.github.io/GroxiStore/",
+    },
+    {
+        id: 9,
+        image: Work9,
+        title: "Quiz App",
+        demo: "https://alaayasser741.github.io/QuizApp/",
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "StartBootstrap",
+        demo: "https://alaayasser741.github.io/BootStarp-Template-one/",
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "Prev Website",
+        demo: "https://alaayasser741.github.io/Template_three/",
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "HooBank Website",
+        demo: "https://alaayasser741.github.io/hoobank/",
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: "Lion Website",
+        demo: "https://alaayasser741.github.io/Html_and_Css_templet_one/",
+    },
+    {
+        id: 8,
+        image: Work8,
+        title: "Aroma Store",
+        demo: "https://nafezly.com/portfolio/37786-Aroma-Store",
+    },
+    {
+        id: 10,
+        image: Work10,
+        title: "Cis Team",
+        demo: "https://alaayasser741.github.io/CIS_UI_Design/ourGroup.html",
+    },
+    {
+        id: 11,
+        image: Work11,
+        title: "Calculator",
+        demo: "https://alaayasser741.github.io/Claculator_JS/",
+    },
+    {
+        id: 12,
+        image: Work12,
+        title: "Tip Calculator App Main",
+        demo: "https://alaayasser741.github.io/tip-calculator-app-main/",
+    },
+    {
+        id: 13,
+        image: Work13,
+        title: "Clipboard landing page",
+        demo: "https://alaayasser741.github.io/Clipboard-landing-page/",
+    },
+    {
+        id: 14,
+        image: Work14,
+        title: "Four Card",
+        demo: "https://alaayasser741.github.io/four-card-feature-section-master/",
+    },
+];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { UilArrowRight } from '@iconscout/react-unicons';
+import './portfolio.css';
+import { Data } from './Data';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section className="portfolio container section">
+      <h2 className="section__title" title='Portfolio Section Title'>Portfolio</h2>
+      <span className="section__subtitle">Most recent work</span>
+      <Swiper
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+        className="portfolio__container">
+        {Data.map(({ id, image, title, demo }) => {
+          return (
+            <SwiperSlide className="portfolio__card" key={id}>
+              <img src={image} alt="Profile-image" title={title} className="portfolio__img" />
+              <div className="portfolio__info">
+                <h3 className="portfolio__name">{title}</h3>
+                <a href={demo} title={title + ' Link'} className="portfolio__demo" target="_blank">Demo <UilArrowRight className="portfolio__demo-icon" size="1rem" /></a>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
   )
 }
 
