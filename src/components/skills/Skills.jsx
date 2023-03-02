a3fbd35c05f293c9534644293a25217be6b9dd45
Alaa Yasser <alaayasser2018@gmail.com>
2023-03-02 15:37:22 +0200

    add Contact component & add Email js to get mails

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ import CS from './CS';
 const Skills = () => {
   return (
     <section className='skills section' id='Skills'>
-      <h2 className="section__title" title='About Section Title'>Skills</h2>
+      <h2 className="section__title" title='Skills Section Title'>Skills</h2>
       <span className="section__subtitle">My technical level</span>
       <div className="skills__container container grid">
         <Frontend />
