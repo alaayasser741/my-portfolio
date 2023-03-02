7cb0eecca6fb6af700172c47d4da9d3f08204a1a
Alaa Yasser <alaayasser2018@gmail.com>
2023-03-02 16:59:16 +0200

    add more comments

--- src/components/testimonial/Data.jsx
+++ src/components/testimonial/Data.jsx
@@ -1,7 +1,9 @@
 import Image1 from "../../assets/testimonial1.png"
 import Image2 from "../../assets/testimonial2.png"
 import Image3 from "../../assets/testimonial3.png"
-
+import Image4 from "../../assets/testimonial4.jpg"
+import Image5 from "../../assets/testimonial5.jpg"
+import Image6 from "../../assets/testimonial6.jpg"
 export const Data = [
     {
         id: 1,
@@ -21,6 +23,27 @@ export const Data = [
         id: 3,
         image: Image3,
         title: "Sara Cill",
+        description:
+            "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+        id: 4,
+        image: Image4,
+        title: "Emma Och",
+        description:
+            "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+        id: 5,
+        image: Image5,
+        title: "Mohamed Ali",
+        description:
+            "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+        id: 6,
+        image: Image6,
+        title: "Kris Talon",
         description:
             "A really good job, all aspects of the project were followed step by step and with good results.",
     },
