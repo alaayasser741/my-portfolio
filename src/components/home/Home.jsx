898b5747a3430831cff998523fa7553b4b46968b
Alaa Yasser <alaayasser2018@gmail.com>
2023-10-24 01:54:39 +0300

    Add palestine Flag

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './home.css';
 import Social from './Social';
 import Data from './Data';
+import flag from '../../assets/palestine.webp'
 import ScrollDown from './ScrollDown';
 const Home = () => {
     return (
         <section className="home section" id="Home">
+        <img className='palestine' src={flag} alt="palestine" />
             <div className="container home__container grid">
                 <div className="home__content grid">
                     <Social />
